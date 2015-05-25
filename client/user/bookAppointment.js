88415fd7dc8d068de588220fe7af15d1a5d22f0b
Innocent <innocentudeh@gmail.com>
2015-05-25 07:17:13 +0000

    validated the appointment form

--- client/user/bookAppointment.js
+++ client/user/bookAppointment.js
@@ -1,22 +1,33 @@
 Template.bookAppointment.events = {
     "click #book" : function(e,t) {
         e.preventDefault();
-       var nameOfPatience =  $("#nametxt").val();
+       var nameOfPatient =  $("#nametxt").val();
+       var phoneOfPatient =  $("#phonetxt").val();
        var description =  $("#bookingDescription").val();
-       var emailOfPatience = $("#emailtxt").val();
+       var emailOfPatient = $("#emailtxt").val();
        var hospital = $("#address").val();
-       var subject = nameOfPatience + "  booked for " + hospital;
+       var subject = nameOfPatient + "  booked for " + hospital;
+
+       if(nameOfPatient === "" && emailOfPatient === ""  && phoneOfPatient === ""){
+        console.log("empty");
+        sAlert.error('Oops! Are you missing something?', {effect: 'genie', position: 'bottom', timeout: 2000});
+      
+       }
+       else{
+
 
        var emailObject = {
             name: subject,
             description: description,
-            hisEmail: emailOfPatience
+            hisEmail: emailOfPatient
        };
        console.log("about to hit the meteor call");
       Meteor.call("sendEmail", emailObject);
 
         Router.go('/');
        swal({   title: "Your request has been sent!",   text: "We will send you a confirmation mail",  type: "success",  timer: 4000,   showConfirmButton: false });
+       }
+
           // return sAlert.success('Request has been sent');
     }
 
